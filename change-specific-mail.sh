9b63b996e3167f9419d9ccdc66427c9fbcd6128a
Alexander Hartmann <38313840+movative@users.noreply.github.com>
2021-01-24 15:27:59 +0100

    Altered change specific

--- change-specific-mail.sh
+++ change-specific-mail.sh
@@ -1,16 +1,16 @@
-$hide="mail@i-want-to-hide.com"
-$name="Alexander Hartmann"
-$mail="38313840+movative@users.noreply.github.com" #(Keep my email adresses private) from https://github.com/settings/emails
+export hide="mail@i-want-to-hide.com"
+export name="Alexander Hartmann"
+export mail="38313840+movative@users.noreply.github.com" #(Keep my email adresses private) from https://github.com/settings/emails
 
 git filter-branch --env-filter "
-if [ $GIT_COMMITTER_EMAIL = $hide ]
+if [ $GIT_COMMITTER_EMAIL = '$hide' ]
 then
-    export GIT_COMMITTER_NAME=$name
-    export GIT_COMMITTER_EMAIL=$mail
+    export GIT_COMMITTER_NAME='$name'
+    export GIT_COMMITTER_EMAIL='$mail'
 fi
-if [ $GIT_AUTHOR_EMAIL = $hide ]
+if [ $GIT_AUTHOR_EMAIL = '$hide' ]
 then
-    export GIT_AUTHOR_NAME=$name
-    export GIT_AUTHOR_EMAIL=$mail
+    export GIT_AUTHOR_NAME='$name'
+    export GIT_AUTHOR_EMAIL='$mail'
 fi
 " --tag-name-filter cat -- --branches --tags
