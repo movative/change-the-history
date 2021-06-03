394b1fa8c5ff911d70c4c8e1d8debae66700e82d
Alexander Hartmann <38313840+movative@users.noreply.github.com>
2021-06-03 17:07:04 +0200

    Add Parameter for Repo, Add verbose echo, Remove specific Mail

--- change-complete-history.sh
+++ change-complete-history.sh
@@ -1,4 +1,10 @@
-export new_name='Alexander Hartmann'
-export new_mail='38313840+movative@users.noreply.github.com'
+export new_name=$(git config --global user.name)
+export new_mail=$(git config --global user.email)
+echo Replacing the user in all commits of $(git remote)/$(git rev-parse --abbrev-ref HEAD) to $new_name, $new_mail
+read  -n 1 -p "Press Any Key to continue"
+if [ $1 ]
+then
+cd $1
+fi
 git filter-branch -f --env-filter "GIT_AUTHOR_NAME='$new_name'; GIT_AUTHOR_EMAIL='$new_mail'; GIT_COMMITTER_NAME='$new_name'; GIT_COMMITTER_EMAIL='$new_mail';" HEAD
-read  -n 1 -p "Press Any Key"
+read  -n 1 -p "Press Any Key to continue"
